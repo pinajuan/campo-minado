6f4622a07816ddc278b900fd7c7e574efeae8110
Juan Pina <juanpinaribeiro@outlook.com>
2024-07-16 23:17:31 -0300

    Classe 'Tabuleiro'

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,5 +1,22 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Tabuleiro {
 
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+	}
+	
 }
