759dff9ce875c48e7e2066791d13be1e3ff5c8c8
Juan Pina <juanpinaribeiro@outlook.com>
2024-07-17 03:00:02 -0300

    [TABULEIRO CONSOLE] Corrigindo distribuição das minas

--- src/br/com/cod3r/cm/visao/TabuleiroConsole.java
+++ src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -67,7 +67,7 @@ public class TabuleiroConsole {
 				}
 				
 			}
-			
+			System.out.println(tabuleiro);
 			System.out.println("Você ganhou!!!");
 		} catch (ExplosaoException e) {
 			System.out.println(tabuleiro);
